b8f94d86cf2e8ed5a1befa27b161db07a9b32df1
pavel.larin <Pavel.Larin@waveaccess.global>
2023-02-17 14:19:48 +0500

    Added an interface to implement the DI pattern

--- src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
+++ src/main/java/com/example/employeecrudapplication/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.employeecrudapplication.controller;
 
 import com.example.employeecrudapplication.model.EmployeeDto;
 import com.example.employeecrudapplication.service.EmployeeService;
+import com.example.employeecrudapplication.service.EmployeeServiceImp;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
